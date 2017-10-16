937b8b6478636482b52dd41a3021edd2f8a0685c
Ramona Comanescu <com.ramona@yahoo.com>
2017-10-16 19:05:13 +0100

    gender machine

---
+++ SexMachine-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='SexMachine',
+    version='0.1.1',
+    author='Ferhat Elmas',
+    author_email='elmas.ferhat@gmail.com',
+    packages=['sexmachine', 'sexmachine.test'],
+    package_dir={'sexmachine': 'sexmachine'},
+    package_data={'sexmachine': ['data/*.txt']},
+    url='https://github.com/ferhatelmas/sexmachine/',
+    license='GPLv3',
+    description='Get the gender from first name.',
+    long_description="Readme",
+)
