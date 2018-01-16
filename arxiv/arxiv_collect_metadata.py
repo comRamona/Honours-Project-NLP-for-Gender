ec1f8665677eeb6e0a26dd63300fdefaa2e88fde
comRamona <com.ramona@yahoo.com>
2018-01-16 22:55:32 +0000

    collect arxiv metadata script

--- arxiv/arxiv_collect_metadata.py
+++ arxiv/arxiv_collect_metadata.py
@@ -1,247 +1,147 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# arXiv harvester - Fetches metadata from arXiv for article metadata.
+#------------------------------------------------------------------------------
+# Copyright (c) 2015 University of Helsinki
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#------------------------------------------------------------------------------
 
-import os
-import time
-import datetime
-import logging
+import urllib.request
+import urllib.parse
+import xml.etree.ElementTree as ET
 import argparse
-import json
+import time
+import sys
+import os
 
-import pymongo
-import requests
-from lxml import etree
-import dateutil.parser
+#------------------------------------------------------------------------------
 
+# Harvest from given set, e.g. cs=computer science
+# see http://export.arxiv.org/oai2?verb=ListSets
+harvest_set = "cs"
 
-logger = logging.getLogger(__name__)
+# Harvest result format, see http://arxiv.org/help/oa/index
+# and http://export.arxiv.org/oai2?verb=ListMetadataFormats
+harvest_format = "arXiv"
 
+# Base API url
+harvest_base_url = "http://export.arxiv.org/oai2"
 
-def first(extracted, require=False):
-    if len(extracted) > 0:
-        return extracted[0]
-    if require:
-        raise RuntimeError('Value is not specified')
+# API request for getting records, see documentation at
+# http://www.openarchives.org/OAI/2.0/openarchivesprotocol.htm#ListRecords
+harvest_url = harvest_base_url + "?verb=ListRecords" \
+              "&set={0}&metadataPrefix={1}". \
+              format(harvest_set, harvest_format)
 
+harvest_url_continue = harvest_base_url + "?verb=ListRecords&resumptionToken={0}"
 
-def parse_metadata_arXiv(record_element):
-    ns = {
-        'oai': 'http://www.openarchives.org/OAI/2.0/',
-        'arxiv': 'http://arxiv.org/OAI/arXiv/',
-    }
-    
-    header_element = record_element.find('oai:header', namespaces=ns)
-    metadata_element = record_element.find('oai:metadata', namespaces=ns)
-    if header_element is None or metadata_element is None:
-        return
-    
-    arxiv_element = metadata_element.find('arxiv:arXiv', namespaces=ns)
-    
-    if arxiv_element is None:
-        return
-    
-    oai_id = first(header_element.xpath('oai:identifier/text()', namespaces=ns))
-    oai_datestamp = first(header_element.xpath('oai:datestamp/text()', namespaces=ns))
-    oai_specs = [subel.text for subel in header_element.findall('oai:setSpec', namespaces=ns)]
-    
-    arxiv_id = first(arxiv_element.xpath('arxiv:id/text()', namespaces=ns), require=True)
-    
-    authors_element = arxiv_element.find('arxiv:authors', namespaces=ns)
-    authors = []
-    for author_element in authors_element.findall('arxiv:author', namespaces=ns):
-        keyname = first(author_element.xpath('arxiv:keyname/text()', namespaces=ns))
-        forenames = first(author_element.xpath('arxiv:forenames/text()', namespaces=ns))
-        authors.append({
-                'keyname': keyname, 
-                'forenames': forenames,
-                'name': ' '.join(author_element.xpath('*/text()', namespaces=ns)),
-            })
-    title = first(arxiv_element.xpath('arxiv:title/text()', namespaces=ns), require=True)
-    abstract = first(arxiv_element.xpath('arxiv:abstract/text()', namespaces=ns), require=True).strip()
-    categories = first(arxiv_element.xpath('arxiv:categories/text()', namespaces=ns), require=True)
-    categories = filter(lambda s: len(s) > 0, categories.split(' ')) 
-    
-    info = {}
-    for subelement in arxiv_element:
-        tag = subelement.xpath('local-name()')
-        if tag not in ('id', 'title', 'authors', 'categories', 'abstract'):
-            info[tag] = subelement.text
-    
-    return {
-        'oai_id': oai_id,
-        'oai_datestamp': oai_datestamp,
-        'oai_specs': oai_specs,
-        
-        'arxiv_id': arxiv_id,
-        'title': title,
-        'authors': authors,
-        'categories': categories,
-        'abstract': abstract,
-        
-        'info': info,
-    }
+sleep_time = 20
 
+#------------------------------------------------------------------------------
 
-def parse_metadata_arXivRaw(record_element):
-    ns = {
-        'oai': 'http://www.openarchives.org/OAI/2.0/',
-        'arxiv': 'http://arxiv.org/OAI/arXivRaw/',
-    }
-    
-    header_element = record_element.find('oai:header', namespaces=ns)
-    metadata_element = record_element.find('oai:metadata', namespaces=ns)
-    if header_element is None or metadata_element is None:
-        return
-    
-    arxiv_element = metadata_element.find('arxiv:arXivRaw', namespaces=ns)
-    if arxiv_element is None:
-        return
-    
-    arxiv_id = first(arxiv_element.xpath('arxiv:id/text()', namespaces=ns), require=True)
-    
-    submitter = first(arxiv_element.xpath('arxiv:submitter/text()', namespaces=ns))
-    
-    versions = []
-    for version_element in arxiv_element.findall('arxiv:version', namespaces=ns):
-        version = version_element.attrib['version']
-        
-        date = first(version_element.xpath('arxiv:date/text()', namespaces=ns))
-        size = first(version_element.xpath('arxiv:size/text()', namespaces=ns))
-        date = dateutil.parser.parse(date).strftime('%Y-%m-%d %H:%M:%S')
-        versions.append({'version': version, 'size': size, 'date': date})
-    
-    return {
-        'arxiv_id': arxiv_id,
-        'submitter': submitter,
-        'versions': versions,
-    }
+def fetch(resumptionToken = "", part=0, from_date="", outdir=""):
+    # Set to starting url, or use the resumptionToken if that is set
+    get_url = harvest_url
 
+    # Set from date if given
+    if from_date:
+        get_url += "&from=" + from_date
 
-def collect_metadata(metadata_collection, metadata_dir):
-    
-    """
-    logger.info('Start reading arXiv metadata')
-    
-    next_metadata_file_id = 0
-    while True:
-        filename = os.path.join(
-            metadata_dir, 
-            'records_arXiv_%.10d.xml' % next_metadata_file_id,
-        )
+    # If a resumption token is given, we use that only
+    if resumptionToken:
+        get_url = harvest_url_continue.format(urllib.parse.quote(resumptionToken))
 
-        if not os.path.exists(filename):
-            break
+    try:
+        # Request from API
+        print("GET", get_url)
+        response = urllib.request.urlopen(get_url)
+        data = response.read()
 
-        logger.info('Processing %s' % filename)
+        # Parse XML
+        root = ET.fromstring(data)
 
-        tree = etree.parse(filename)
-        record_elements = tree\
-            .getroot()\
-            .find('{http://www.openarchives.org/OAI/2.0/}ListRecords')\
-            .findall('{http://www.openarchives.org/OAI/2.0/}record')
+        # Get resumptionToken element from the XML
+        namespaces = {'oai': 'http://www.openarchives.org/OAI/2.0/'}
+        rtNode = root.find('./oai:ListRecords/oai:resumptionToken', namespaces)
 
-        metadata_objects = []
-        for record_element in record_elements:
-            obj = parse_metadata_arXiv(record_element)
-            if obj is not None:
-                obj['_id'] = obj.pop('arxiv_id')
-                metadata_objects.append(obj)
-            else:
-                record_xml = etree.tostring(record_element)
-                logger.error(
-                    'Cannot parse metadata record'
-                    'in {filename}: {record_xml}'.format(**locals())
-                )
+        cursor = 0
+        resumptionToken = None
 
-        metadata_collection.insert_many(metadata_objects)
-        logger.info('Inserted %d records' % len(metadata_objects))
+        if rtNode is not None:
+            resumptionToken = rtNode.text
+            cursor = rtNode.attrib['cursor']
 
-        next_metadata_file_id += 1
+        # Form filename
+        if outdir != '' and outdir[-1] != '/':
+            outdir += '/'
+        filename = outdir + "arxiv-{0}-{1}-cursor{2}.xml".format(harvest_set,
+                                                                 harvest_format,
+                                                                 cursor)
+        # Write to file
+        print("Writing to", filename)
+        with open(filename, 'wb') as fp:
+            fp.write(data)
 
-    """    
-    logger.info('Start reading arXivRaw metadata')
-    
-    next_metadata_file_id = 0
-    while True:
-        filename = os.path.join(
-            metadata_dir, 
-            'records_arXivRaw_%.10d.xml' % next_metadata_file_id,
-        )
+        if not resumptionToken:
+            print("No resumptionToken, stopping ...")
+            return 1
 
-        if not os.path.exists(filename):
-            break
+        print("Sleeping {0} seconds ...".format(sleep_time))
+        time.sleep(sleep_time)
+        return fetch(resumptionToken, part + 1, "", outdir)
 
-        logger.info('Processing %s' % filename)
+    except urllib.error.HTTPError as error:
+        # If we are fetching too fast the server will give an HTTP 503
+        # then we just wait the specified time and try again
+        if error.code == 503:
+            h = error.headers
+            if 'retry-after' in h:
+                ra = int(h['retry-after'])
+                print("Got HTTP 503, Retry-after: {0}".format(ra))
+                print("Sleeping {0} seconds ...".format(ra))
+                time.sleep(ra)
+                return fetch(resumptionToken, part, from_date)
 
-        tree = etree.parse(filename)
-        record_elements = tree\
-            .getroot()\
-            .find('{http://www.openarchives.org/OAI/2.0/}ListRecords')\
-            .findall('{http://www.openarchives.org/OAI/2.0/}record')
+        # Any other errors than 503 are fatal.
+        print("ERROR: HTTP returned status {0}!".format(rstat))
+        return 1
 
-        for record_element in record_elements:
-            obj = parse_metadata_arXivRaw(record_element)
-            if obj is not None:
-                arxiv_id = obj.pop('arxiv_id')
-                metadata_collection.update_one(
-                    {'_id': arxiv_id},
-                    {'$set': obj},
-                )
-            else:
-                record_xml = etree.tostring(record_element)
-                logger.error(
-                    'Cannot parse raw metadata record'
-                    'in {filename}: {record_xml}'.format(**locals())
-                )
+#------------------------------------------------------------------------------
 
-        next_metadata_file_id += 1
-
-
-def write_to_jsonlines_file(metadata_collection, jsonlines_file):  
-    logger.info('Writing metadata to jsonlines file')
-    records = list(metadata_collection.find({}, {'_id': True, 'info.created': True}))
-    
-    logger.info('Sort records in jsonlines file by ???')
-    records.sort(key=lambda record: '%s %s' % (record['info']['created'], record['_id']))
-    
-    for n, record in enumerate(records):
-        metadata_record = metadata_collection.find_one(record['_id'])
-        jsonlines_file.write(json.dumps(metadata_record, separators=(',', ':')) + '\n')
-        if n % 1000 == 0 and n > 0:
-            logger.info('Writed %d records' % n)
-    
-        
 if __name__ == '__main__':
-    logging.basicConfig(
-        format='[%(asctime)s] %(levelname)s %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        level=logging.DEBUG,
-    )
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--db', default='mongodb://localhost:27017/arxiv')
-    parser.add_argument('--drop-collection', default=False, action='store_true')
-    parser.add_argument('--read-metadata-dir')
-    parser.add_argument('--write-jsonlines-file', type=argparse.FileType('w'))
+    parser.add_argument('-r', '--resumptionToken', metavar='TOKEN')
+    parser.add_argument('-d', '--outdir', metavar='DIR', 
+                        help='place xml files in this directory')
+    parser.add_argument('-f', '--from', dest='from_date', metavar='DATE',
+                        help='Fetch records updated since FROM datestamp, ' + 
+                        'e.g. "2014-12-01"')
     args = parser.parse_args()
-    
-    client = pymongo.MongoClient(args.db)
-    db_uri_parts = pymongo.uri_parser.parse_uri(args.db)
-    db_name = db_uri_parts['database']
-    collection_name = db_uri_parts['collection'] or 'metadata'
 
-    arxiv_db = client[db_name]
-    if args.drop_collection:
-        arxiv_db.drop_collection(collection_name)    
-    metadata_collection = arxiv_db[collection_name]
-    
-    if args.read_metadata_dir:
-        collect_metadata(
-            metadata_collection,
-            metadata_dir=args.read_metadata_dir,
-        )
-    
-    if args.write_jsonlines_file:
-        write_to_jsonlines_file(
-            metadata_collection,
-            args.write_jsonlines_file,
-        )
+    if args.resumptionToken:
+        fetch(args.resumptionToken, outdir=args.outdir)
+    elif args.from_date:
+        fetch("", 0, args.from_date, outdir=args.outdir)
+    else:
+        fetch(outdir=args.outdir)
